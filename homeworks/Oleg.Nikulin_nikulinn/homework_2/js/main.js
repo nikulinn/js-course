103cb9c55d4383b3bd2cb3e78884fa6c26350782
nikulinn <nikulinn1991@gmail.com>
2016-11-15 20:56:48 +0200

    fix bug with undefind and change tabulation

--- homeworks/Oleg.Nikulin_nikulinn/homework_2/js/main.js
+++ homeworks/Oleg.Nikulin_nikulinn/homework_2/js/main.js
@@ -1,68 +1,72 @@
 (function () {
   // 1-st create empty array
 
-  var array = [];
-  console.log("Create empty array :");
-  console.log(array);
+    var array = [],
+        newData = [2, 12, 22, 6, 32],
+        oldData = [12, 5, 7, 17, 22, 32];
+
+    console.log("Create empty array :");
+    console.log(array);
 
   // 2-d add 100 intager to array with mathod random
 
-  function createArray() {
-    array = new Array(100);
-    for (i=0; i<100; ++i) {
-      array[i] = Math.round(Math.random()*100);
-    }
-    // console.log(array.length);
-  };
+    function createArray() {
+        array = new Array(100);
+        for (i=0; i<100; ++i) {
+            array[i] = Math.round(Math.random()*100);
+        }
+        return array;
+    };
 
-  createArray();
-  console.log("Array with intager :");
-  console.log(array);
+    createArray();
+    console.log("Array with intager :");
+    console.log(array);
 
   // 3-d search min and max value in array
 
-  function findMaxMin(value) {
-    if (value == 'resultMax') {
-      return Math.max.apply(null,array);
-    } else if (value == 'resultMin') {
-      return Math.min.apply(null,array);
-    }
-  };
-  console.log("Max value from array : " + findMaxMin('resultMax'));
-  console.log("Min value from array : " + findMaxMin('resultMin'));
+    function findMaxMin(value) {
+        if (value == 'resultMax') {
+            return Math.max.apply(null,array);
+        } else if (value == 'resultMin') {
+            return Math.min.apply(null,array);
+        }
+    };
+
+    console.log("Max value from array : " + findMaxMin('resultMax'));
+    console.log("Min value from array : " + findMaxMin('resultMin'));
 
   // 4-th convert array to object
 
-  function arrayToObject() {
-    var object = {};
-      for (var i = 0; i <= array.length; i++) {
-        if (i <= 25) {
-          object[String.fromCharCode(i + 97)] = array[i];
-        } else {
-          var numb = i - 25;
-          object[numb] = array[i];
+    function arrayToObject(array) {
+        var object = {};
+        for (var i = 0; i < array.length; i++) {
+            if (i <= 25) {
+                object[String.fromCharCode(i + 97)] = array[i];
+            } else {
+                var numb = i - 25;
+                object[numb] = array[i];
+            }
         }
-      }
-    return object;
-  }
-  console.log("Converted object from array :");
-  console.log(arrayToObject());
+        return object;
+    };
+
+    console.log("Converted object from array :");
+    console.log(arrayToObject(array));
 
   // 5-th make filter
 
-  function filterNew(arr1,arr2) {
-    for (var i = 0; i < oldData.length; i++) {
-      for (var j = newData.length - 1; j !== 0 ; j--) {
-        if (newData[j] == oldData[i]) {
-          newData.splice(j,1);
+    function filterNew(arr1,arr2) {
+        for (var i = 0; i < oldData.length; i++) {
+            for (var j = newData.length - 1; j !== 0 ; j--) {
+                if (newData[j] == oldData[i]) {
+                    newData.splice(j,1);
+                }
+            }
         }
-      }
-    }
-  }
+    };
 
-   var newData = [2, 12, 22, 6, 32];
-   var oldData = [12, 5, 7, 17, 22, 32];
    filterNew(newData, oldData);
+
    console.log("New filtered array : " + newData);
 
 })();
